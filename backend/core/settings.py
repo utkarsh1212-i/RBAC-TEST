0f9ac31020c47133fe49c12dd2b64d7dac35951a
Utkarsh BAiragi <utkarshui1212@gmail.com>
2025-01-05 18:47:06 +0530

    Refactor email settings to use environment variables for credentials in settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -91,10 +91,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'utkarshui1212@gmail.com'  # Use environment variable for security
-EMAIL_HOST_PASSWORD = 'xfmr rvdt gsgq tinj'  # Use app-specific password for Gmail
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Use environment variable for security
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use app-specific password for Gmail
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Use environment variable for security
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use app-specific password for Gmail
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
